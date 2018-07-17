322aa438a71290fd78a3635d8dee16243e37bffb
Janaina Militão <janaina.militao@gmail.com>
2018-07-17 17:24:13 -0300

    Cadastro de email Slick

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -32,27 +32,15 @@ class PersonController @Inject()(repo: PersonRepository,
 
 
   def addPerson = Action.async { implicit request =>
-    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle succes.
-    personForm.bindFromRequest.fold(
-      // The error function. We return the index page with the error form, which will render the errors.
-      // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
-      // a future because the person creation function returns a future.
-      errorForm => {
-        Future.successful(Ok(views.html.index(errorForm)))
-      },
-      // There were no errors in the from, so create the person.
+    personForm.bindFromRequest.fold(errorForm => {
+      Future.successful(Ok(views.html.index(errorForm)))
+    },
       person => {
-        repo.create(person.name, person.age).map { _ =>
-          // If successful, we simply redirect to the index page.
-          Redirect(routes.PersonController.index).flashing("success" -> "user.created")
-        }
+        repo.create(person.name, person.age)
+        Future.successful(Redirect(routes.PersonController.index()).flashing("success" -> "user.created"))
       }
     )
   }
-
-  /**
-    * A REST endpoint that gets all the people as JSON.
-    */
   def getPersons = Action.async { implicit request =>
     repo.list().map { people =>
       Ok(Json.toJson(people))
@@ -60,11 +48,4 @@ class PersonController @Inject()(repo: PersonRepository,
   }
 }
 
-/**
-  * The create person form.
-  *
-  * Generally for forms, you should define separate objects to your models, since forms very often need to present data
-  * in a different way to your models.  In this case, it doesn't make sense to have an id parameter in the form, since
-  * that is generated once it's created.
-  */
 case class CreatePersonForm(name: String, age: Int)
